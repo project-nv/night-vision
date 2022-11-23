fbc7e385f6ff27bfe3dfadd610c2c375ee2db899
BoilingSoup <boilingsoup@protonmail.com>
2022-11-23 12:22:28 -0800

    update Scripts interface

--- types/core/scripts.d.ts
+++ types/core/scripts.d.ts
@@ -1,5 +1,7 @@
+// not sure what is internal/external here
+
 export interface Scripts {
-  init(srcs: any): void;
+  // init(srcs: any): void; // internal ?
   srcLib: any[];
   prefabs: { [key: string]: any };
   parse(): void;
