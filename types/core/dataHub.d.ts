216a47c10726ed53cfdbc0cf3f2a53315f9022c0
BoilingSoup <boilingsoup@protonmail.com>
2022-11-27 06:22:44 -0800

    update DataHub interface

--- types/core/dataHub.d.ts
+++ types/core/dataHub.d.ts
@@ -4,15 +4,39 @@ import DataView$ from "./dataView";
 // TODO: add method doc strings
 // TODO: fix any
 export interface DataHub {
+  /** Data object */
+  readonly data: any;
+
+  /** Index based mode */
+  readonly indexBased: boolean;
+
+  /** Pane with the main overlay (main pane) */
+  readonly chart: Pane;
+
+  readonly offchart: Pane[] | any; // Not sure if Pane[] is correct.
+  // `any` is insurance.
+
+  /** Main overlay ref */
+  readonly mainOv: any;
+
+  /** Main pane ID */
+  readonly mainPaneId: number;
+
+  /** Create a subset of timeseries */
   filter(data: Data, range: any, offset?: number): DataView$;
 
+  /** Get all active panes (with UUID) */
   panes(): Pane[];
 
+  /** Get overlay ref by paneId & ovId */
   overlay(paneId: number, ovId: number): Overlay; // returns a single overlay?
 
+  /** Get overlaya data by paneId & ovId */
   ovData(paneId: number, ovId: number): OverlayData;
 
+  /** Get overlay data subset by paneId & ovId */
   ovDataSubset(paneId: number, ovId: number): OverlayDataSubset;
 
-  allOverlays(): Overlay[];
+  /** Get All overlays */
+  allOverlays(type: any): Overlay[];
 }
