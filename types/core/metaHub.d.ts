8bdbefe3f03198dbd6968da203ee932c8de8a2d5
BoilingSoup <boilingsoup@protonmail.com>
2022-11-27 06:14:59 -0800

    update MetaHub interface

--- types/core/metaHub.d.ts
+++ types/core/metaHub.d.ts
@@ -1,3 +1,5 @@
+import { Overlay } from "../types";
+
 export interface MetaHub {
   /** Legend formatters */
   readonly legendFns: any[];
@@ -17,9 +19,26 @@ export interface MetaHub {
   /** Price labels + price lines */
   readonly valueTrackers: any[];
 
+  readonly selectedOverlay: Overlay;
+
+  /** OHLC map of the main Overlay */
+  readonly ohlcMap: any[];
+
+  /** OHLC mapper function */
+  readonly ohlcFn: any;
+
+  /** Get y-transform of a specific scale */
   getYtransform(gridId: number, scaleId: number): any;
 
+  /** Get auto precision of a specific overlay */
+  getAutoPrec(gridId: number, ovId: number): any;
+
+  /** Get a precision sampler of a specific overlay */
   getPreSampler(gridId: number, ovId: number): any;
 
+  /** Get legend formatter of a specific overlay */
   getLegendFns(gridId: number, ovId: number): any;
+
+  /** Get OHLC values to use as "magnet" values */
+  ohlc(t: any): any;
 }
