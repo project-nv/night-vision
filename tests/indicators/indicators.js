990e6ca97d211c01890213af9b0cb06115c30eed
c451 <codeallright@protonmail.com>
2022-12-17 13:54:19 +0400

    Change ind-test, add indicator showcase

--- tests/indicators/indicators.js
+++ tests/indicators/indicators.js
@@ -1,4 +1,6 @@
 
+import Utils from '../../src/stuff/utils.js'
+
 const FILES = import.meta.glob(
     '../../data/indicators/*.json', { eager: true })
 
@@ -6,24 +8,44 @@ let IND = {}
 
 for (var file in FILES) {
     let [fn, ext] = file.split('.json')
-    IND[fn] = FILES[file].default
+    IND[fn.split('/').pop().split('-')[0]] = FILES[file].default
 }
-console.log(IND)
+
 export default function test(stack, chart) {
 
     stack.startTest('All indicators')
 
-    for (var fn in IND) {
-        stack.add(`Set ${fn}`, ($fn => {
-            return () => {
-                chart.data = IND[$fn]
+    stack.add(`Choose indicator to show`, () => {
+        chart.data = IND['rsi']
+        chart.fullReset()
+        chart.se.uploadAndExec()
+
+        console.log(
+            '%c Call ind(<indicator-name>) \n or showcase()',
+            'font-size: 1.25em; color: orange;')
+
+        window.ind = (name) => {
+            name = name.toLowerCase()
+            if (!IND[name]) {
+                console.warn('There is no indicator ' + name + '\n',
+                `Indicator list: \n ${Object.keys(IND)}`)
+                return
+            }
+            chart.data = IND[name]
+            chart.fullReset()
+            chart.se.uploadAndExec()
+        }
+
+        window.showcase = async (delay = 1000) => {
+            for (var name in IND) {
+                chart.data = IND[name]
                 chart.fullReset()
                 chart.se.uploadAndExec()
+                await Utils.pause(delay)
             }
-        })(fn))
-        stack.stop()
-    }
-
+        }
+    })
+    stack.stop()
     stack.endTest()
 
 }
